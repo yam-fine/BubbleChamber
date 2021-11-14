e91dcc50fb1540b5518e775add61c4cd23c43f94
Matans94 <shomermatan@gmail.com>
2021-11-15 01:11:57 +0200

    GameManager Update
    with stack overflow :/

--- ChainBoi/Assets/Scripts/GameManager.cs
+++ ChainBoi/Assets/Scripts/GameManager.cs
@@ -2,14 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] public TMP_Text scoreText;
+    [SerializeField] public GameObject spawnPointsParent;
+    [SerializeField] public Transform player;
+    [SerializeField] public float spawnRadiusFromObj; // spawn outside this radius of all objects (players and enemy)
+    [SerializeField] public float sightRadius; // radius to disable spawns.
+    [SerializeField] public float validRadius; // limit radius from player to spawn.
+    
     private static TextMeshProUGUI uiscore;
     public static int score;
+    private STAGE gameStage;
+    private bool[] spawnsOnBoard;
+    
+    private enum STAGE
+    {
+        TOTURIAL,
+        STAGE1
+    };
 
     static GameManager instance;
     public static GameManager Instance {
@@ -25,18 +40,24 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         uiscore = scoreText.GetComponent<TextMeshProUGUI>();
-        
-        
-        //uiscore.SetText("Score: 0");
-        
+        gameStage = STAGE.TOTURIAL;
         uiscore.text = "Score: 0";
         score = 0;
+        DisableAllFood();
+        SpawnPoints();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (gameStage)
+        {
+            case STAGE.TOTURIAL:
+                
+                
+            case STAGE.STAGE1:
+                break;
+        }
     }
 
     public static void IncScore()
@@ -44,6 +65,62 @@ public class GameManager : MonoBehaviour
         score++;
         uiscore.text = ("Score: " + score);
     }
+
+    private void DisableAllFood()
+    {
+        for (int i = 0; i < spawnPointsParent.transform.childCount; i++)
+        {
+            spawnPointsParent.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    //spawn new food to the game. assume there is available object to spawn. 
+    private void SpawnPoints()
+    {
+        Vector3 position = FindPosToSpawn();
+        int indexToSpawn = FindIndexToSpawn();
+        if (indexToSpawn == -1)
+        {
+            Debug.Log("No object to spawn in hierarchy! ");
+        }
+        
+        Transform spawn = spawnPointsParent.transform.GetChild(indexToSpawn);
+        spawn.transform.position = position;
+        spawn.gameObject.SetActive(true);
+    }
+
     
+    private int FindIndexToSpawn()
+    {
+        for (int i = 0; i < spawnPointsParent.transform.childCount; i++)
+        {
+            if (!spawnPointsParent.transform.GetChild(i).gameObject.activeSelf)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
     
+    //find available pos to spawn.
+    private Vector3 FindPosToSpawn()
+    {
+        var spawnPosition = (player.transform.position + ((validRadius * Random.insideUnitSphere)));
+        
+        if (Vector3.Distance(spawnPosition, player.transform.position) <= spawnRadiusFromObj)
+        {
+            return FindPosToSpawn();
+        }
+
+        for (int i = 0; i < spawnsOnBoard.Length; i++)
+        {
+            if (spawnPointsParent.transform.GetChild(i).gameObject.activeSelf &&
+                Vector3.Distance(spawnPointsParent.transform.GetChild(i).position, spawnPosition)
+                <= spawnRadiusFromObj)
+                return FindPosToSpawn();
+        }
+
+        return spawnPosition;
+    }
 }
